0272021423c8e2a3e7578991cd3f5ff88d6c3dd3
S1L3NT388 <oleksandr.o.solynskyi@ukd.edu.ua>
2026-04-28 18:39:33 +0300

    Завдання долар

---
+++ task2.js
@@ -0,0 +1,7 @@
+function showUserInfo(name, age, city, country) {
+    const info = `User: ${name}, Age: ${age}, City: ${city}, Country: ${country}`;
+    
+    console.log(info);
+}
+
+showUserInfo("Sasha", 18, "Ivano-Frankivsk", "Ukraine");
